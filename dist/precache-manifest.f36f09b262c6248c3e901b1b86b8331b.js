2cffb68f777d47127539e07b2914365af5766745
v.akmaev <v.akmaev@QPRM-D8TX2F3>
2022-06-01 15:27:18 +0400

    release

---
+++ dist/precache-manifest.f36f09b262c6248c3e901b1b86b8331b.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9238feff842a10086b78",
+    "url": "/css/Home.4b429cdb.css"
+  },
+  {
+    "revision": "90e99e83ddf35f5c0687",
+    "url": "/css/app.b5db6066.css"
+  },
+  {
+    "revision": "659951b036d5b4d1c4e5c84ff2cb0b1f",
+    "url": "/img/BEP20.659951b0.png"
+  },
+  {
+    "revision": "659951b036d5b4d1c4e5c84ff2cb0b1f",
+    "url": "/img/ETHEREUM.659951b0.png"
+  },
+  {
+    "revision": "ee95975de7c19db09448598e4646286d",
+    "url": "/img/exchange.ee95975d.png"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/img/icons1/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/img/icons1/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/img/icons1/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/img/icons1/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/img/icons1/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/img/icons1/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img/icons1/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/img/icons1/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img/icons1/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img/icons1/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/img/icons1/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/img/icons1/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/icons1/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/img/icons1/mstile-150x150.png"
+  },
+  {
+    "revision": "6de16254d1b4193353e7015f9177535d",
+    "url": "/img/icons1/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "535bdfc0200e43dfe065d71bea5a0239",
+    "url": "/img/telegram-logo-small.535bdfc0.png"
+  },
+  {
+    "revision": "4c86351b7e83691ec21aca213a3195b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9238feff842a10086b78",
+    "url": "/js/Home.229ff55e.js"
+  },
+  {
+    "revision": "90e99e83ddf35f5c0687",
+    "url": "/js/app.c81d74be.js"
+  },
+  {
+    "revision": "6cb44503f3d807a017e5",
+    "url": "/js/chunk-vendors.ebbaa042.js"
+  },
+  {
+    "revision": "c76ef1a4fe51b49fb2adf4c35f6bf8d6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "730455a53efc6615cfd6bad0f5037e14",
+    "url": "/static/favicon.png"
+  }
+]);
